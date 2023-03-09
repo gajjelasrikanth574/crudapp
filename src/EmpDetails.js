e2e5bf4f77edbfda1057ff9e23c972dfa77317cf
srikanthchinnu01 <srikanthchinnu01@gamil.com>
2023-03-09 22:18:21 +0530

    my crud app

---
+++ src/EmpDetails.js
@@ -0,0 +1,42 @@
+import { useEffect,useState } from "react";
+import { Link, useParams } from "react-router-dom";
+
+const EmpDetails=()=>{
+    const {empid}=useParams()
+    const [empdata,empdatachange]=useState({})
+
+    useEffect(()=>{
+        fetch(" http://localhost:3001/employee/"+empid).then((res)=>{
+            return res.json();
+        }).then((resp)=>{
+            empdatachange(resp)
+
+        }).catch((err)=>{
+            console.log(err.message);
+        })
+
+
+    },[])
+    return(
+        <div>
+            <div className="card" style={{"textAlign":"left"}}>
+                            <div className="card-title">
+                                <h2>Employee Create</h2>
+                            </div>
+                            <div className="card-body"></div>
+                        
+            {  empdata &&
+            <div>
+                <h2>the Employee Name is :<b>{empdata.name}</b> ({empdata.id})</h2>
+                <h3>conatct details </h3>
+                <h5>Email is : {empdata.email}</h5>
+                <h5>Phone is : {empdata.phone}</h5>
+                <Link className="btn btn-danger" to="/">Back to Listing</Link>
+            </div>
+          
+            }
+                </div>
+        </div>
+    )
+}
+export default EmpDetails;
